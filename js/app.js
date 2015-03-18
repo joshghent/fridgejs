bc1a6aebf11253d9d2586657465a512b5877ef94
Josh Ghent <me@joshghent.com>
2015-03-18 02:07:30 +0000

    Basic functionality now operating correctly

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 var main = function(){
+
+  $('.btn').click(function(){
+    var add = $('.magnet-box').val();
+    $('<li>').text(add).prependTo('.magnets');
+    $('.magnet-box').val('');
+    $('.counter').text('0');
+  });
   $('.magnet-box').keyup(function(){
     var magnetLength = $(this).val().length;
     var characterAmount = 0 + magnetLength;

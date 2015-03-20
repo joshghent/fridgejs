8ee2ec20240bb74b4b1296511302044ca32ef8d4
Josh Ghent <me@joshghent.com>
2015-03-20 23:43:16 +0000

    Added instant focus on input

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var main = function(){
 
-  $('.btn').click(function(){
+  $('.btn').click(function submit(){
     var add = $('.magnet-box').val();
     $('<li>').text(add).prependTo('.magnets');
     $('.magnet-box').val('');
@@ -10,6 +10,14 @@ var main = function(){
     var magnetLength = $(this).val().length;
     var characterAmount = 0 + magnetLength;
     $('.counter').text(characterAmount);
+  });
+  $('.magnet-box input').keydown(function(e){
+    if(e.keycode == 13){
+      alert("You pressed enter!");
+    }
+  });
+  $(function(){
+    $('.magnet-box').focus();
   });
 };
 

fafbacc5a4a594c1c43a65c9d37d7282271e63b2
Josh Ghent <me@joshghent.com>
2015-03-28 00:13:07 +0000

    Added refocus on submit. Broke some things

--- js/app.js
+++ js/app.js
@@ -1,22 +1,24 @@
-var main = function(){
+var main = function() {
 
-  $('.btn').click(function submit(){
+  $('.btn').click(function submit() {
     var add = $('.magnet-box').val();
     $('<li>').text(add).prependTo('.magnets');
     $('.magnet-box').val('');
     $('.counter').text('0');
+    $('.magnet-box').focus();
   });
-  $('.magnet-box').keyup(function(){
+  $('.magnet-box').keyup(function() {
     var magnetLength = $(this).val().length;
     var characterAmount = 0 + magnetLength;
     $('.counter').text(characterAmount);
   });
-  $('.magnet-box input').keydown(function(e){
-    if(e.keycode == 13){
-      alert("You pressed enter!");
+  $('.magnet-box').keypress(function(e) {
+    if (e.which == 13) {
+      $('.btn').submit();
+      return false; //<---- Add this line
     }
   });
-  $(function(){
+  $(function() {
     $('.magnet-box').focus();
   });
 };
